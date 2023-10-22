ec7ca80992e12c25d8018099493906233c8d1f77
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-10-22 23:23:44 +0300

    Changed repository url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import GlobalStyle from 'globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/film-finder">
       <GlobalStyle />
       <App />
     </BrowserRouter>
